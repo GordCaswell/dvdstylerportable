ad1fcf3630fb136e832f970ebab01cb285866a99
Gord Caswell <GordCaswell@users.noreply.github.com>
2017-01-20 19:13:07 -0600

    Copy in current official release (3.0.2)

--- App/DVDStyler/bin/ffmpeg-vbr.bat
+++ App/DVDStyler/bin/ffmpeg-vbr.bat
@@ -14,8 +14,6 @@ FOR /F "tokens=1,2 delims==" %%a IN (ff_vbr.ini) DO IF %%a==XHQ_threshold SET /A
 FOR /F "tokens=1,2 delims==" %%a IN (ff_vbr.ini) DO IF %%a==MediumBitrate_threshold SET /A MediumBitrate_threshold=%%b
 FOR /F "tokens=1,2 delims==" %%a IN (ff_vbr.ini) DO IF %%a==LowBitrate_threshold SET /A LowBitrate_threshold=%%b
 FOR /F "tokens=1,2 delims==" %%a IN (ff_vbr.ini) DO IF %%a==HQ_params SET HQ_params=%%b
-FOR /F "tokens=1,2 delims==" %%a IN (ff_vbr.ini) DO IF %%a==AppendLog SET AppendLog=%%b
-FOR /F "tokens=1,2 delims==" %%a IN (ff_vbr.ini) DO IF %%a==LogFolder SET LogFolder=%%b
 
 
 SET ORIGINAL=%*
@@ -24,7 +22,6 @@ SET size=
 SET HQ=NO
 SET height=
 SET CQM=
-SET tempdir=
 
 
 REM -----------------------------------------------------------------------------------------------------------------------------------------------------
@@ -87,53 +84,24 @@ SET ORIGINAL=%ORIGINAL:!=_$$§§$$_%
 SETLOCAL EnableDelayedExpansion
 IF [%force_mode%==[-mode SET ORIGINAL=!ORIGINAL:%force_mode% %mode_value% =!
 
-IF NOT DEFINED LogFolder GOTO DefaultLogFolder
-SET LogFolder=%LogFolder:"=%
-SET LogFolder=%LogFolder%\
-SET LogFolder=%LogFolder:\\=\%
-IF NOT EXIST "%LogFolder%NUL" MD "%LogFolder%"
-SET tempdir=%LogFolder%
-GOTO done
-:DefaultLogFolder
-REG QUERY HKEY_CURRENT_USER\Software\DVDStyler\Generate /v TempDir >"%temp%\temp.txt"
-IF ERRORLEVEL 1 DEL "%temp%\temp.txt" & GOTO portable
-FOR /F "usebackq tokens=3" %%a IN ("%temp%\temp.txt") DO SET tempdir=%%a
-IF EXIST "%temp%\temp.txt" DEL "%temp%\temp.txt"
-GOTO done
-:portable
-FOR /F "usebackq tokens=1,2 delims==" %%a IN ("..\dvdstyler.ini") DO IF %%a==TempDir SET tempdir=%%b
-IF NOT "%tempdir%"=="" SET tempdir=%tempdir:\\=\%
-IF NOT "%tempdir%"=="" GOTO done
-FOR /F "usebackq tokens=1,2 delims==" %%a IN ("%USERPROFILE%\dvdstyler.ini") DO IF %%a==TempDir SET tempdir=%%b
-IF NOT "%tempdir%"=="" SET tempdir=%tempdir:\\=\%
-IF NOT "%tempdir%"=="" GOTO done
-SET tempdir=%temp%\
-
-:done
 IF %VBR_threshold%==1 SET /A VBR_threshold=9000
 IF %HQ_threshold%==1 SET /A HQ_threshold=9000
 IF %XHQ_threshold%==1 SET /A XHQ_threshold=9000
 IF %twopass_threshold%==1 SET /A twopass_threshold=9000
-ECHO.%ORIGINAL% | %SYSTEMROOT%\system32\FIND "entry001">nul
-IF ERRORLEVEL 1 GOTO skip
-IF NOT [%AppendLog%==[1 IF EXIST "%tempdir%ff_vbr.log" DEL "%tempdir%ff_vbr.log"
-IF [%AppendLog%==[1 IF EXIST "%tempdir%ff_vbr.log" (
-	ECHO =================================================================================>>"%tempdir%ff_vbr.log"
-	ECHO. >>"%tempdir%ff_vbr.log"
-	ECHO. >>"%tempdir%ff_vbr.log"
-)
-ECHO FFMpeg-VBR Plugin Settings:>>"%tempdir%ff_vbr.log"
-ECHO ----------------------------------------------------->>"%tempdir%ff_vbr.log"
-ECHO VBR Threshold:               %VBR_threshold% kbps>>"%tempdir%ff_vbr.log"
-ECHO 2-Pass Threshold:            %twopass_threshold% kbps>>"%tempdir%ff_vbr.log"
-ECHO HQ Threshold:                %HQ_threshold% kbps>>"%tempdir%ff_vbr.log"
-ECHO Extreme HQ Threshold:        %XHQ_threshold% kbps>>"%tempdir%ff_vbr.log"
-ECHO Medium Bitrate Threshold:    %MediumBitrate_threshold% kbps>>"%tempdir%ff_vbr.log"
-ECHO Low Bitrate Threshold:       %LowBitrate_threshold% kbps>>"%tempdir%ff_vbr.log"
-ECHO ----------------------------------------------------->>"%tempdir%ff_vbr.log"
-ECHO. >>"%tempdir%ff_vbr.log"
 
-:skip
+ECHO =============================================================================
+ECHO. 
+ECHO FFMpeg-VBR Plugin Settings:
+ECHO -----------------------------------------------------
+ECHO VBR Threshold:               %VBR_threshold% kbps
+ECHO 2-Pass Threshold:            %twopass_threshold% kbps
+ECHO HQ Threshold:                %HQ_threshold% kbps
+ECHO Extreme HQ Threshold:        %XHQ_threshold% kbps
+ECHO Medium Bitrate Threshold:    %MediumBitrate_threshold% kbps
+ECHO Low Bitrate Threshold:       %LowBitrate_threshold% kbps
+ECHO -----------------------------------------------------
+ECHO. 
+
 IF [%bitrate%==[ GOTO dont_touch
 SET /A VBR_threshold=%VBR_threshold% * 1000
 SET /A HQ_threshold=%HQ_threshold% * 1000
@@ -158,8 +126,9 @@ SET ORIGINAL=!ORIGINAL: -g 18 = -g 12 !
 SET ORIGINAL=!ORIGINAL:-maxrate:v:0 %bitrate%=-maxrate:v:0 8500000 -dc 10 -bf 2 -qmin 1 -lmin 0.75 -mblmin 50!
 SET ORIGINAL=!ORIGINAL:-minrate:v:0 %bitrate% =!
 SET ORIGINAL=!ORIGINAL:-c:v:0 mpeg2video=-c:v:0 mpeg2video %CQM%!
+SET HQ_params=%HQ_params:-bf 2=%
 IF %bitrate% LSS %HQ_threshold% SET ORIGINAL=!ORIGINAL:-c:v:0 mpeg2video=-c:v:0 mpeg2video %HQ_params%! & SET HQ=YES
-IF %bitrate% LSS %XHQ_threshold% SET ORIGINAL=!ORIGINAL:-c:v:0 mpeg2video=-c:v:0 mpeg2video %HQ_params%! & SET HQ=YES
+IF "%HQ%"=="NO" IF %bitrate% LSS %XHQ_threshold% SET ORIGINAL=!ORIGINAL:-c:v:0 mpeg2video=-c:v:0 mpeg2video %HQ_params%! & SET HQ=YES
 GOTO dont_touch
 
 
@@ -177,21 +146,20 @@ SET ORIGINAL=!ORIGINAL:maxrate:v:0 %bitrate%=maxrate:v:0 %Fixed_Rate%!
 :NoFix
 SET ORIGINAL=!ORIGINAL:-c:v:0 mpeg2video=-c:v:0 mpeg2video -dc 10 %CQM%!
 IF %bitrate% LSS %HQ_threshold% SET ORIGINAL=!ORIGINAL:-c:v:0 mpeg2video=-c:v:0 mpeg2video %HQ_params%! & SET HQ=YES
-IF %bitrate% LSS %XHQ_threshold% SET ORIGINAL=!ORIGINAL:-c:v:0 mpeg2video=-c:v:0 mpeg2video %HQ_params%! & SET HQ=YES
+IF "%HQ%"=="NO" IF %bitrate% LSS %XHQ_threshold% SET ORIGINAL=!ORIGINAL:-c:v:0 mpeg2video=-c:v:0 mpeg2video %HQ_params%! & SET HQ=YES
 
 :dont_touch
 setlocal DisableDelayedExpansion
 SET ORIGINAL=%ORIGINAL:_$$§§$$_=!%
-ECHO ^<%date%  %time%^> >>"%tempdir%ff_vbr.log"
-ECHO File Name: %infile%>>"%tempdir%ff_vbr.log"
-ECHO Encoding Mode: %mode%   HQ: %HQ%   Custom Quant Matrix: %Matrix%>>"%tempdir%ff_vbr.log"
-ECHO. >>"%tempdir%ff_vbr.log"
-ECHO Executing command generated by the ffmpeg-VBR plugin:>>"%tempdir%ff_vbr.log"
-ECHO ffmpeg.exe %ORIGINAL%>>"%tempdir%ff_vbr.log"
-ECHO. >>"%tempdir%ff_vbr.log"
-ECHO. >>"%tempdir%ff_vbr.log"
-IF DEFINED mode START "%mode% Mode..." /W ffmpeg.exe %ORIGINAL% & GOTO :EOF
-START /W ffmpeg.exe %ORIGINAL%
+ECHO ^<%date%  %time%^> 
+ECHO File Name: %infile%
+ECHO Encoding Mode: %mode%   HQ: %HQ%   Custom Quant Matrix: %Matrix%
+ECHO. 
+ECHO Executing command generated by the ffmpeg-VBR plugin:
+ECHO ffmpeg.exe %ORIGINAL%
+ECHO. 
+ffmpeg.exe %ORIGINAL%
+ECHO. 
 GOTO :EOF
 
 
@@ -202,12 +170,13 @@ SET FIRST=!FIRST: -g 15 = -g 12 !
 SET FIRST=!FIRST: -g 18 = -g 12 !
 SET FIRST=!FIRST:-maxrate:v:0 %bitrate%=-maxrate:v:0 8500000 -dc 10 -bf 2 -q:v 2!
 SET FIRST=!FIRST:-minrate:v:0 %bitrate% =!
-IF %bitrate% LSS %XHQ_threshold% IF EXIST "MPEG Adapted.txt" SET /P CQM=<"MPEG Adapted.txt" & SET Matrix=MPEG Adapted
+IF %bitrate% LSS %XHQ_threshold% IF EXIST "Fox New.txt" SET /P CQM=<"Fox New.txt" & SET Matrix=Fox New
 IF %bitrate% LSS %HQ_threshold% SET HQ_params=-b_strategy 2 -brd_scale 2 -profile:v 4
 IF %bitrate% LSS %XHQ_threshold% SET HQ_params=-b_strategy 2 -brd_scale 2 -profile:v 4
 SET FIRST=!FIRST:-c:v:0 mpeg2video=-c:v:0 mpeg2video %CQM%!
 IF %bitrate% LSS %HQ_threshold% SET FIRST=!FIRST:-c:v:0 mpeg2video=-c:v:0 mpeg2video %HQ_params%! & SET HQ=YES
-IF %bitrate% LSS %XHQ_threshold% SET FIRST=!FIRST:-c:v:0 mpeg2video=-c:v:0 mpeg2video %HQ_params%! & SET HQ=YES (extreme)
+IF "%HQ%"=="NO" IF %bitrate% LSS %XHQ_threshold% SET FIRST=!FIRST:-c:v:0 mpeg2video=-c:v:0 mpeg2video %HQ_params%!
+IF %bitrate% LSS %XHQ_threshold% SET HQ=YES (extreme)
 IF NOT DEFINED out_m2v SET FIRST=!FIRST:%out%=-an -passlogfile "%out_path%ffmpeg" -pass 1 -y "NUL.avi"! & GOTO second
 SET FIRST=!FIRST:%out_m2v%=-an -passlogfile "%out_path%ffmpeg" -pass 1 -y "NUL.avi"!
 SET /A pos=0
@@ -225,26 +194,27 @@ SET SECOND=!SECOND: -g 18 = -g 12 !
 SET SECOND=!SECOND:-maxrate:v:0 %bitrate%=-maxrate:v:0 8500000 -dc 10 -bf 2 -lmin 0.75 -mblmin 50 -qmin 1!
 SET SECOND=!SECOND:-minrate:v:0 %bitrate% =!
 FOR /F "tokens=1,2 delims==" %%a IN (ff_vbr.ini) DO IF %%a==HQ_params SET HQ_params=%%b
+SET HQ_params=%HQ_params:-bf 2=%
 IF %bitrate% LSS %XHQ_threshold% SET HQ_params=-pre_dia_size 5 -dia_size 5 -qcomp 0.7 -qblur 0 -preme 2 -me_method dia -sc_threshold 0 -sc_factor 4 -bidir_refine 4 -profile:v 4 -mbd rd -mbcmp satd -precmp satd -cmp satd -subcmp satd -skipcmp satd
 SET SECOND=!SECOND:-c:v:0 mpeg2video=-c:v:0 mpeg2video %CQM%!
-IF %bitrate% LSS %HQ_threshold% SET SECOND=!SECOND:-c:v:0 mpeg2video=-c:v:0 mpeg2video %HQ_params%!
-IF %bitrate% LSS %XHQ_threshold% SET SECOND=!SECOND:-c:v:0 mpeg2video=-c:v:0 mpeg2video %HQ_params%!
+IF NOT "%HQ%"=="NO" SET SECOND=!SECOND:-c:v:0 mpeg2video=-c:v:0 mpeg2video %HQ_params%!
 IF NOT DEFINED out_m2v SET SECOND=!SECOND:%out%=-passlogfile "%out_path%ffmpeg" -pass 2 %out%! & GOTO twopasslog
 SET SECOND=!SECOND:%out_m2v%=-passlogfile "%out_path%ffmpeg" -pass 2 %out_m2v%!
 
 :twopasslog
 setlocal DisableDelayedExpansion
 SET ORIGINAL=%ORIGINAL:_$$§§$$_=!%
-ECHO ^<%date%  %time%^> >>"%tempdir%ff_vbr.log"
-ECHO File Name: %infile%>>"%tempdir%ff_vbr.log"
-ECHO Encoding Mode: %mode%   HQ: %HQ%   Custom Quant Matrix: %Matrix%>>"%tempdir%ff_vbr.log"
-ECHO. >>"%tempdir%ff_vbr.log"
-ECHO Executing first pass command generated by the ffmpeg-VBR plugin:>>"%tempdir%ff_vbr.log"
-ECHO ffmpeg.exe %FIRST%>>"%tempdir%ff_vbr.log"
-ECHO. >>"%tempdir%ff_vbr.log"
-ECHO Executing second pass command generated by the ffmpeg-VBR plugin:>>"%tempdir%ff_vbr.log"
-ECHO ffmpeg.exe %SECOND%>>"%tempdir%ff_vbr.log"
-ECHO. >>"%tempdir%ff_vbr.log"
-ECHO. >>"%tempdir%ff_vbr.log"
-START "%mode% Mode - Executing First Pass..." /W ffmpeg.exe %FIRST%
-START "%mode% Mode - Executing Second Pass..." /W ffmpeg.exe %SECOND%
+ECHO ^<%date%  %time%^> 
+ECHO File Name: %infile%
+ECHO Encoding Mode: %mode%   HQ: %HQ%   Custom Quant Matrix: %Matrix%
+ECHO. 
+ECHO Executing first pass command generated by the ffmpeg-VBR plugin:
+ECHO ffmpeg.exe %FIRST%
+ECHO. 
+ffmpeg.exe %FIRST%
+ECHO. 
+ECHO Executing second pass command generated by the ffmpeg-VBR plugin:
+ECHO ffmpeg.exe %SECOND%
+ECHO. 
+ffmpeg.exe %SECOND%
+ECHO. 
